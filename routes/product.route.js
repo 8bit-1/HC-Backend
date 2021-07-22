415de321f262b97cd433bebba5b1bca49363b513
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-07-21 20:48:47 -0600

    Update product.route.js
    se creó la ruta para obtener productos

--- routes/product.route.js
+++ routes/product.route.js
@@ -5,5 +5,6 @@ const productController = require('../controllers/product.controller');
 
 router.route('/:idUser').post(productController.createProduct);
 router.route('/update/:idProduct').post(productController.updateProduct);
+router.route('/getAllProducts').get(productController.getProducts);
 
 module.exports = router;
