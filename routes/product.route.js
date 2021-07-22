8ebc4a9f8c4f94ba8b683216a84383c3742e90bb
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-07-21 22:05:27 -0600

    Update product.route.js
    se agregarón los parametros inicio y ranngo a la ruta de obtener productos.
    
    se creó la ruta obtener productos para el usuario loggueado.

--- routes/product.route.js
+++ routes/product.route.js
@@ -5,6 +5,7 @@ const productController = require('../controllers/product.controller');
 
 router.route('/:idUser').post(productController.createProduct);
 router.route('/update/:idProduct').post(productController.updateProduct);
-router.route('/getAllProducts').get(productController.getProducts);
+router.route('/getAllProducts/:init/:range').get(productController.getProducts);
+router.route('/getAllProducts/:idUser/:init/:range').get(productController.getProductsLogged);
 
 module.exports = router;
