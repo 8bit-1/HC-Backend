4423852218303a5bed93096093eaf7ddc790f39d
Jorge-Marin <jorgemarin9915@gmail.com>
2021-07-29 17:42:22 -0600

    Restored the exception written by el giesy

--- util/errorFunctions.js
+++ util/errorFunctions.js
@@ -1,14 +1,9 @@
 function excepcion(mensaje, error) {
-  return {
-    'message': mensaje,
-    "error": error.code,
-    "stack": error.stack,
-  }
-  // this.message = mensaje;
-  // this.error = error;
-  // this.toString = function () {
-  //   return this.message + this.error;
-  // };
+  this.message = mensaje;
+  this.error = error;
+  this.toString = function () {
+    return this.message + this.error;
+  };
 }
 
 module.exports = {
