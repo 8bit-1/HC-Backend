f4dbc2633fd68f31f422a576a9921580a14a440d
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-08-10 15:00:16 -0600

    ruta para denuncias
    se agrego las rutas para denunciar

---
+++ routes/complaint.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { AuthToken } = require('../middlewares/tokenAuth');
+
+const complaintController = require('../controllers/complaint.controller');
+
+router.route('/product/:idProduct').all(AuthToken).post(complaintController.reportProducts);
+router.route('/user/:accused').all(AuthToken).post(complaintController.reportUser);
+router.route('/comments/:idComentary').all(AuthToken).post(complaintController.reportComments);
+
+module.exports=router;
