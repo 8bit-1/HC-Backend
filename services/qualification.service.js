eb786f747ffbf59339a680052ca5ebf8e1e01c6a
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-08-10 00:35:32 -0600

    Servicios para like
    se agrego el controlador para obtener likes de un usuario verificar si ya dio like, dar like y quitar like

--- services/qualification.service.js
+++ services/qualification.service.js
@@ -19,6 +19,70 @@ const qualificationProduct = async function (qualification, idUser, idProduct) {
   }
 };
 
+const giveLike = async function (idUser, Seller) {
+  var query = `call rateUser(?,?)`;
+  try {
+    const [estado] = await db.execute(query, [ 
+           idUser,
+           Seller
+           
+    ]);
+
+    if (estado.affectedRows != 0)
+      return (message = "like created sucessfully");
+
+    return estado;
+  } catch (error) {
+    throw Error("Error while Creating like: " + error);
+  }
+};
+
+const disLike = async function (idUser, Seller) {
+  var query = `call unrateUser(?,?)`;
+  try {
+    const [estado] = await db.execute(query, [ 
+           idUser,
+           Seller
+           
+    ]);
+
+    if (estado.affectedRows != 0)
+      return (message = "dislike created sucessfully");
+
+    return estado;
+  } catch (error) {
+    throw Error("Error while Creating dislike: " + error);
+  }
+};
+
+const getLikeUser = async function (idUser) {
+  const query = `SELECT count(*) likes from usercalification where Seller=?`;
+  try {
+    let [estado] = await db.execute(query, [
+           idUser           
+    ]);
+    return estado;
+  } catch (error) {
+    throw Error("Error while get like: " + error);
+  }
+};
+
+const verifyLike = async function (idUser, Seller) {
+  const query = `SELECT count(*) likes from usercalification where Buyer=? and Seller=? `;
+  try {
+    let [estado] = await db.execute(query, [
+           idUser, Seller          
+    ]);
+    return estado;
+  } catch (error) {
+    throw Error("Error while get like: " + error);
+  }
+};
+
 module.exports={
-    qualificationProduct
+    qualificationProduct,
+    getLikeUser,
+    verifyLike,
+    giveLike,
+    disLike
 };
