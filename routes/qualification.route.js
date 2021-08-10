edd691b9bf9084394c432b739c08829c02bdcd5a
Carlos Daniel Zambrano <58059760+CarlosZambrano1999@users.noreply.github.com>
2021-08-10 00:35:16 -0600

    Rutas para like
    se agrego las rutas para obtener likes de un usuario verificar si ya dio like, dar like y quitar like.

--- routes/qualification.route.js
+++ routes/qualification.route.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { AuthToken } = require('../middlewares/tokenAuth');
 
 const qualificationController = require('../controllers/qualification.controller');
 
 router.route('/product/:idUser/:idProduct').post(qualificationController.qualificationProduct);
+router.route('/likesUser/:idUser').get(qualificationController.getLikeUser);
+router.route('/verifyLike/:Seller').all(AuthToken).get(qualificationController.verifyLike);
+router.route('/giveLike/:Seller').all(AuthToken).post(qualificationController.giveLike);
+router.route('/disLike/:Seller').all(AuthToken).post(qualificationController.disLike);
 
 module.exports=router;
